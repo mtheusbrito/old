d84bed691d686a41c1c9e4d291c2d1eddc8270e1
matheus <matheusbritodasilva@gmail.com>
2019-03-19 13:43:49 -0300

    form pronto, bug: não envia 2 vezes, necessitando reload apos primeiro submit

---
+++ assets/js/form_submission-handler.js
@@ -0,0 +1,111 @@
+(function () {
+
+
+
+
+    function validEmail(email) {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        return re.test(email);
+    }
+
+
+
+    function getFormData(form) {
+        var elements = form.elements;
+
+        var fields = Object.keys(elements).filter(function (k) {
+            return (elements[k].name !== "honeypot");
+        }).map(function (k) {
+            if (elements[k].name !== undefined) {
+                return elements[k].name;
+                // Edge 
+            } else if (elements[k].length > 0) {
+                return elements[k].item(0).name;
+            }
+        }).filter(function (item, pos, self) {
+            return self.indexOf(item) == pos && item;
+        });
+
+        var formData = {};
+        fields.forEach(function (name) {
+            var element = elements[name];
+
+
+            formData[name] = element.value;
+
+
+            if (element.length) {
+                var data = [];
+                for (var i = 0; i < element.length; i++) {
+                    var item = element.item(i);
+                    if (item.checked || item.selected) {
+                        data.push(item.value);
+                    }
+                }
+                formData[name] = data.join(', ');
+            }
+        });
+
+
+        formData.formDataNameOrder = JSON.stringify(fields);
+        formData.formGoogleSheetName = form.dataset.sheet || "responses";
+        formData.formGoogleSendEmail = form.dataset.email || "";
+
+        console.log(formData);
+        return formData;
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        var form = event.target;
+        var data = getFormData(form);
+
+
+
+        if (data.email && !validEmail(data.email)) {  //email invalido
+            var invalidEmail = form.querySelector(".email-invalid");
+            if (invalidEmail) {
+                invalidEmail.style.display = "block";
+                return false;
+            }
+        } else {
+            disableAllButtons(form);
+            var url = form.action;
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', url);
+            // xhr.withCredentials = true;
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function () {
+                console.log(xhr.status, xhr.statusText);
+                console.log(xhr.responseText);
+                form.reset();
+
+
+                swal("Obrigado!", "Te retornarei em breve!", "success");
+                return;
+            };
+
+            var encoded = Object.keys(data).map(function (k) {
+                return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+            }).join('&');
+            xhr.send(encoded);
+        }
+    }
+
+    function loaded() {
+        console.log("Contact form submission handler loaded successfully.");
+        //bind submit evento
+        var forms = document.querySelectorAll("form.gform");
+        for (var i = 0; i < forms.length; i++) {
+            forms[i].addEventListener("submit", handleFormSubmit, false);
+        }
+    };
+    document.addEventListener("DOMContentLoaded", loaded, false);
+
+    function disableAllButtons(form) {
+        var buttons = form.querySelectorAll("button");
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+        }
+    }
+})();
